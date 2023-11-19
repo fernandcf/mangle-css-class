6bdbf8818f5e9fa949838084b80f90d8abd8848c
Fernando R <124534259+fernandcf@users.noreply.github.com>
2023-11-19 16:59:52 -0500

    Delete apps/vite-vanilla/.mangle-css-class directory

--- apps/vite-vanilla/.mangle-css-class/classes.js
+++
@@ -1,5 +0,0 @@
-const logo = "logo_q45pV";
-const vanilla = "vanilla_rjaXf";
-const card = "card_EmSXv";
-const read_the_docs = "read-the-docs_nJPNi";
-export default {logo,vanilla,card,read_the_docs}
