5d3fbe376d750814a32025bd59fbec0fe2682699
Fernando R <dev.fernandocf@gmail.com>
2023-11-20 12:16:42 -0500

    update

--- apps/vite-vanilla/.mangle-css-class/classes.js
+++ apps/vite-vanilla/.mangle-css-class/classes.js
@@ -1,5 +1,5 @@
-const logo = "logo_q45pV";
-const vanilla = "vanilla_rjaXf";
-const card = "card_EmSXv";
-const read_the_docs = "read-the-docs_nJPNi";
+const logo = "logo_a3bMC";
+const vanilla = "vanilla_Rooha";
+const card = "card_gAg8R";
+const read_the_docs = "read-the-docs_xpgTi";
 export default {logo,vanilla,card,read_the_docs}
